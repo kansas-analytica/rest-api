09d135f0fe0c40b3a884d36eb8ebecfb1699b21f
Damian Vu <contact@damianvu.com>
2019-03-19 19:26:09 +0000

    Pulled models from database. Registered with app. Creating views next

--- api_app/models.py
+++ api_app/models.py
@@ -1,4 +1,23 @@
 from django.db import models
 
+class Tweets(models.Model):
+    created_at = models.DateTimeField()
+    id_str = models.CharField(max_length=255, primary_key=True)
+    text = models.TextField()
+    user_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'tweets'
 
 
+class TwitterAccounts(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    screen_name = models.CharField(max_length=255)
+    description = models.TextField()
+    date_created = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'twitter_accounts'
--- restapi/settings.py
+++ restapi/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api_app'
+    'api_app',
 ]
 
 MIDDLEWARE = [
