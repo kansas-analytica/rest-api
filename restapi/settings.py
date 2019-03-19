95f0d88c949468531fe4e674127ae331025f0098
Damian Vu <contact@damianvu.com>
2019-03-19 19:03:07 +0000

    Adjusted for our current database. Put secret key on server

--- restapi/settings.py
+++ restapi/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8$6$x8fg2pive(qg_gk+4o#7ifsij!_j))lc(&j6cw1xi%#n_b'
+with open('/home/api/secretkey.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api.kansasanalytica.com']
 
 
 # Application definition
